7c8f3af478bf0a26e383c4c2fd2f7b05abf29a56
Anushka <anushkamommy16@gmail.com>
2026-07-16 00:57:29 +0530

    Add basic.py with practice solutions

---
+++ basic.py
@@ -0,0 +1,38 @@
+# 1. Write a python program to add two numbers.
+# 2. Write a python program to find remainder when a number is divided by z.
+# 3. Check the type of variable assigned using input() function.
+# 4. Use comparison operator to find out whether ‘a’ given variable is greater than ‘b’ or not.
+# Take a = 34 and b = 80
+# 5. Write a python program to find an average of two numbers entered by the user.
+# 6. Write a python program to calculate the square of a number entered by the user.
+
+#SOLUTIONS
+
+#1. 
+# num1=int(input("Enter num: "))
+# num2=int(input("Enter num: "))
+# print(f"The sum is {num1+num2}")
+
+#2
+# divid=int(input("Enter num that is to be divided: "))
+# div=int(input("Enter num that divides: "))
+# print(f"The remainder is {divid % div}")
+
+#3
+# userInput=input("Enter a data: ")
+# print(type(userInput))
+
+#4
+# a = 34 
+# b = 80
+# print(a>b)
+# print(a<b)
+
+#5
+# num1=int(input("Enter num: "))
+# num2=int(input("Enter num: "))
+# print(f"The average is {(num1+num2)/2}")
+
+#6
+# num1=int(input("Enter num: "))
+# print(f"The square of {num1} is {num1**2} ")
