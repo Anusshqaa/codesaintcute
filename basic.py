84cb41005ad1df34287077aba455c1e1a83cedff
Anushka <anushkamommy16@gmail.com>
2026-07-16 01:17:34 +0530

    basics2 add

--- basic.py
+++ basic.py
@@ -36,3 +36,42 @@
 #6
 # num1=int(input("Enter num: "))
 # print(f"The square of {num1} is {num1**2} ")
+
+
+# 2. Write a program to fill in a letter template given below with name and date.
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+# 3. Write a program to detect double space in a string.
+# 4. Replace the double space from problem 3 with single spaces.
+
+
+#SOLUTIONS
+
+
+#2. 
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+# name=input("Enter name: ")
+# date=input("Enter date: ")
+# letter=letter.replace("<|Name|>", name)
+# letter=letter.replace("<|Date|>", date)
+# print(letter)
+
+#3
+# text = "This string contains  double spaces."
+# DetectSpace=text.find("  ")
+# if DetectSpace != -1 :
+#     print(f"Detected at {DetectSpace}")
+# else:
+#     print("Not Detected")
+
+#4
+# text = "This string contains  double spaces."
+# NewTxt=text.replace("  ", " ")
+# print(NewTxt)
