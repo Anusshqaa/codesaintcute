73e3f63bfdd0d5be7571c37c11b309c1e6efd7c3
Anushka <anushkamommy16@gmail.com>
2026-07-15 20:49:37 +0530

    Fix time logic: rock beats scissors

--- rps.py
+++ rps.py
@@ -1,18 +1,21 @@
 import random
 case=["Rock", "Paper", "Scissor"]
 UserChoice=int(input("Enter your choice : 0 for Rock. 1 for Paper and 2 for Scissors- "))
-print(f"User chose: {case[UserChoice]}")
-Compchoice=random.randint(0,2)
-print(f"Computer chose: {case[Compchoice]}")
 if UserChoice >=3 or UserChoice < 0: 
     print("Invalid.")
-elif Compchoice > UserChoice :
-    print("You lose.")
-elif Compchoice < UserChoice :
-    print("You win.")
-elif Compchoice==2 and UserChoice==0:
-    print("You win.")
-elif Compchoice==0 and UserChoice==2:
-    print("You lose.")
-elif Compchoice==UserChoice:
-    print("Draw.")
+else: 
+    print(f"User chose: {case[UserChoice]}")
+    Compchoice=random.randint(0,2)
+    print(f"Computer chose: {case[Compchoice]}")
+    if UserChoice >=3 and UserChoice < 0 :
+        print("Inalid choice.")
+    elif Compchoice==2 and UserChoice==0:
+        print("You win.")
+    if Compchoice > UserChoice :
+        print("You lose.")
+    elif Compchoice==0 and UserChoice==2:
+        print("You lose.")
+    elif Compchoice < UserChoice :
+        print("You win.")
+    elif Compchoice==UserChoice:
+        print("Draw.")
